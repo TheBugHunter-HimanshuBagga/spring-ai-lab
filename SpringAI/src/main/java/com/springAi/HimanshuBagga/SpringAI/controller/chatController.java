a47752d2ff7ce7ed0c9923afae1be0780eb215e3
TheBugHunter-HimanshuBagga <baggahimanshu311@gmail.com>
2026-07-04 01:43:43 +0530

    Used OPENAI-API_KEY to get output from openAI

---
+++ SpringAI/src/main/java/com/springAi/HimanshuBagga/SpringAI/controller/chatController.java
@@ -0,0 +1,25 @@
+package com.springAi.HimanshuBagga.SpringAI.controller;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
+public class chatController {
+
+    private ChatClient chatClient; // all the functions that needs to connected to thee openAi
+
+    public chatController(ChatClient.Builder builder){
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public ResponseEntity<String> chat(@RequestParam(value = "q" , required = true) String q){ // q is the question asked by the user
+        var resultResponse = chatClient.prompt(q).call().content();
+        return ResponseEntity.ok(resultResponse);
+    }
+}
